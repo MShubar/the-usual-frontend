2b6da4cd066eedbc8b056e27351080fa98bf8418
dev08 <dev08@saluberdev.com>
2025-10-20 16:42:19 +0300

    apibakend edited

--- src/views/API.jsx
+++ src/views/API.jsx
@@ -1,4 +1,4 @@
-// export const API_BACKEND =
-//   'https://the-usual-backend-gnavf2dkdebghhh3.canadacentral-01.azurewebsites.net'
+export const API_BACKEND =
+  'https://the-usual-backend-gnavf2dkdebghhh3.canadacentral-01.azurewebsites.net'
 
-export const API_BACKEND = "http://localhost:8080" 
+// export const API_BACKEND = "http://localhost:8080" 
